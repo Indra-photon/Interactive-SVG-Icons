4b3c95778a19f63c3d187524762cebffb7a56572
Indranil Maiti <indranilmaiti1@gmail.com>
2026-07-15 12:39:54 +0200

    added card travel

---
+++ components/craftui/blocks/travel-card/examples/default-preview.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import TravelCard from "../default";
+
+export default function DefaultPreview() {
+  return (
+    <div className="flex h-full w-full items-center justify-center bg-white dark:bg-neutral-950">
+      <TravelCard />
+    </div>
+  );
+}
